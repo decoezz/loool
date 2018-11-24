e999e501f9170bacf81ec256776c5512131d5ee4
decoezz <45102309+decoezz@users.noreply.github.com>
2018-11-24 12:35:57 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -76,4 +76,4 @@ credit credit credit credit credit credit credit credit credit credit credit cre
  
  
  
-client.login(NTAwMDI3MzI0OTk0MTU4NjIz.DtXNrA.lP8k5cqcW9s8uAZywIGKKDE6fE8);
+client.login(TOKEN);
